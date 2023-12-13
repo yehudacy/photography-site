beda00b219693e9e8c689862eebd68820e3672ef
yehudacy <126159312+yehudacy@users.noreply.github.com>
2023-12-13 15:29:17 +0200

    scrips commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('connected!')
+})
+
+
+
+
+app.listen(process.env.PORT);
