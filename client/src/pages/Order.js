f2ef8f3a4d26898946d276126de0f8b14330182a
yehudacy <126159312+yehudacy@users.noreply.github.com>
2023-12-18 08:28:35 +0200

    order alert and dialog commit

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { TextField, Button, Typography, Grid, Box } from "@mui/material";
+import { TextField, Button, Typography, Grid, Box, Alert, AlertTitle } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import dayjs from "dayjs";
 import { Navigate, useLocation } from "react-router-dom";
+import MyDialog from "../components/MyDialog";
 
 const Order = () => {
-  const { state} = useLocation();
-  
+  const { state } = useLocation();
+
 
   const [orderDate] = useState(dayjs());
   const [actionDate, setActionDate] = useState(dayjs());
@@ -16,36 +17,64 @@ const Order = () => {
   // const [email, setEmail] = useState("");
   const [price] = useState(state?.packagePrice);
   const [remarks, setRemarks] = useState("");
-  if(!state)  {
-    return <Navigate to={'/pricing'}/>
+  const [dialog, setDialog] = useState(false);
+  const [alert, setAlert] = useState("");
+
+  if (!state) {
+    return <Navigate to={'/pricing'} />
   }
+
+  //content for dialog
+  const dialogText = {
+    dialogContent: "Do you want to check-out",
+    dialogBtn1Text: "go to payment",
+    dialogBtn2Text: "back",
+  }
+
+
+  const handlepayments = () => {
+    console.log("got to go t payments page!")
+  }
+
+  const handleCloseDialog = () => {
+    setDialog(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //
+    setDialog(true);
   };
 
   return (
-    <Box sx={{ height: "100vh" }}>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ height: "100%" }}
-      >
-        <Grid item xs={12} md={4}>
-          <Box sx={{ p: 2 }}>
-            <Typography variant="h4" align="center" mb={2}>
-              Order details
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={2}>
-                {/* <Grid item xs={12} md={6}>
+    <>
+      {dialog && (
+        <MyDialog
+          dialogText={dialogText}
+          open={dialog}
+          handleAgree={handlepayments}
+          handleCloseDialog={handleCloseDialog}
+        />
+      )}
+      <Box sx={{ height: "100vh" }}>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ height: "100%" }}
+        >
+          <Grid item xs={12} md={4}>
+            <Box sx={{ p: 2 }}>
+              <Typography variant="h4" align="center" mb={2}>
+                Order details
+              </Typography>
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                  {/* <Grid item xs={12} md={6}>
                   
                 </Grid> */}
-                <Grid item  xs={12} md={6} sx={{m: 'auto'}} >
-                  {/* <TextField
+                  <Grid item xs={12} md={6} sx={{ m: 'auto' }} >
+                    {/* <TextField
                     fullWidth
                     label="Order Date"
                     value={orderDate}
@@ -53,7 +82,7 @@ const Order = () => {
                     margin="normal"
                     required
                   /> */}
-                  {/* <TextField
+                    {/* <TextField
                     fullWidth
                     label="Phone Number"
                     value={phoneNumber}
@@ -61,28 +90,28 @@ const Order = () => {
                     margin="normal"
                     required
                   /> */}
-                  <DatePicker 
-                    sx={{width: '100%', marginY: 2}}
-                    label='order date'
-                    value={orderDate}
-                    readOnly
-                  />
-                  <DatePicker 
-                    sx={{width: '100%', marginY: 2}}
-                    label='action date'
-                    disablePast
-                    value={actionDate}
-                    onChange={(newDate) => setActionDate(newDate)}
-                  />
-                   <TimePicker
-                    sx={{width: '100%', marginY: 2}}
-                    label="execution time"
-                    ampm={false}
-                    disablePast
-                    value={time}
-                    onChange={(newTime) =>  setTime(newTime)}
-                  />
-                  {/* <TextField
+                    <DatePicker
+                      sx={{ width: '100%', marginY: 2 }}
+                      label='order date'
+                      value={orderDate}
+                      readOnly
+                    />
+                    <DatePicker
+                      sx={{ width: '100%', marginY: 2 }}
+                      label='action date'
+                      disablePast
+                      value={actionDate}
+                      onChange={(newDate) => setActionDate(newDate)}
+                    />
+                    <TimePicker
+                      sx={{ width: '100%', marginY: 2 }}
+                      label="execution time"
+                      ampm={false}
+                      disablePast
+                      value={time}
+                      onChange={(newTime) => setTime(newTime)}
+                    />
+                    {/* <TextField
                     fullWidth
                     label="Email"
                     value={email}
@@ -91,35 +120,44 @@ const Order = () => {
                     required
                     type="email"
                   /> */}
-                  <TextField
-                    fullWidth
-                    label="Price"
-                    value={`${price} NIS`}
-                    // onChange={(e) => setEmail(e.target.value)}
-                    margin="normal"
-                    required
-                    type="text"
-                  />
-                  <TextField
-                    fullWidth
-                    label="Remarks"
-                    value={remarks}
-                    onChange={(e) => setRemarks(e.target.value)}
-                    margin="normal"
-                    required
-                    multiline
-                    rows={4}
-                  />
-                  <Button variant="contained" type="submit" sx={{ mt: 2 }} fullWidth>
-                    Place order
-                  </Button>
+                    <TextField
+                      fullWidth
+                      label="Price"
+                      value={`${price} NIS`}
+                      // onChange={(e) => setEmail(e.target.value)}
+                      margin="normal"
+                      required
+                      type="text"
+                    />
+                    <TextField
+                      fullWidth
+                      label="Remarks"
+                      value={remarks}
+                      onChange={(e) => setRemarks(e.target.value)}
+                      margin="normal"
+                      required
+                      multiline
+                      rows={4}
+                    />
+                    <Button variant="contained" type="submit" sx={{ marginY: 2 }} fullWidth>
+                      Place order
+                    </Button>
+                    {alert && (alert === 'error' ? <Alert severity="error">
+                      <AlertTitle>{alert}</AlertTitle>
+                      It didn't work out — <strong>please try again!</strong>
+                    </Alert> : 
+                    <Alert severity="success">
+                    <AlertTitle>{alert}</AlertTitle>
+                    order has been placed! — <strong>Thank you! we will be in touch if there is any problems</strong>
+                  </Alert>)}
+                  </Grid>
                 </Grid>
-              </Grid>
-            </form>
-          </Box>
+              </form>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   )
 }
 
--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -4,11 +4,14 @@ const ordersRouter = express.Router();
 
 //place a new order
 ordersRouter.post('/', async (req, res) => {
-    try{
-        console.log(req.body);
-        res.status(201).json({message: "the order processed as expected"});
-    } catch(error) {
-
+    try {
+        //validation if failed send with status 400(bad request)
+        //handle order table in sql
+        //handle sending me an email
+        //if success
+        res.status(201).json({ message: "the order processed as expected" });
+    } catch (error) {
+        res.status(500).json({message:"The server is down please try again later"}) 
     }
 })
 
