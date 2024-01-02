4f3112c48060e95110d710c9cea5ffa9f5c462e6
yehudacy <126159312+yehudacy@users.noreply.github.com>
2024-01-02 02:03:56 +0200

    contact me client delete

--- client/src/components/ContactRequestsTable.js
+++ client/src/components/ContactRequestsTable.js
@@ -10,6 +10,8 @@ import {
   IconButton,
   Grid,
   Typography,
+  Alert,
+  AlertTitle,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import axiosInstance from "../axiosInstance";
@@ -21,7 +23,7 @@ const ContactRequestsTable = () => {
   useEffect(() => {
     const fetchAllContactMe = async () => {
       try {
-        const {data} = await axiosInstance.get("/contactme");
+        const { data } = await axiosInstance.get("/contactme");
         if (data.length === 0) {
           setContactMeErrorMsg("No contact me requests to be found");
         }
@@ -34,56 +36,85 @@ const ContactRequestsTable = () => {
     fetchAllContactMe();
   }, []);
 
-  const onDelete = () => {};
+  const onDelete = async (index) => {
+    try {
+      const { data } = await axiosInstance.delete(`/contactme/${contactRequests[index].contact_me_id}`);
+      let newArr = [...contactRequests];
+      const filteredArr = newArr.filter((item) => {
+        return !(item.contact_me_id === contactRequests[index].contact_me_id)
+      });
+      console.log(filteredArr)
+      if (!filteredArr.length) {
+        setContactMeErrorMsg("No contact me requests to be found");
+      }
+      setContactRequests(filteredArr);
+    } catch (error) {
+      setContactMeErrorMsg(error.message);
+      setTimeout(() => {
+        setContactMeErrorMsg("")
+      console.log(error)
+    }, 6000)
+  }
+};
 
-  return (
-    <Grid item xs={12} md={9}>
-      <Typography variant="h4" gutterBottom>
-        Contact Me Requests
-      </Typography>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ width: "20%" }}>Name</TableCell>
-              <TableCell style={{ width: "20%" }}>Phone Number</TableCell>
-              <TableCell style={{ width: "20%" }}>Email</TableCell>
-              <TableCell style={{ width: "30%" }}>Message</TableCell>
-              <TableCell style={{ width: "10%" }}>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {contactRequests.length > 0 ? (
-              contactRequests.map((request, index) => (
-                <TableRow key={index}>
-                  <TableCell>{request.name}</TableCell>
-                  <TableCell>{request.phone_number}</TableCell>
-                  <TableCell>{request.email}</TableCell>
-                  <TableCell>{request.message}</TableCell>
-                  <TableCell>
-                    <IconButton
-                      onClick={() => onDelete(index)}
-                      color="secondary"
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={5}>
-                  <Typography variant="h6" color="error">
-                    {contactMeErrorMsg }
-                  </Typography>
+return (
+  <Grid item xs={12} md={9}>
+    <Typography variant="h4" gutterBottom>
+      Contact Me Requests
+    </Typography>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell style={{ width: "20%" }}>Name</TableCell>
+            <TableCell style={{ width: "20%" }}>Phone Number</TableCell>
+            <TableCell style={{ width: "20%" }}>Email</TableCell>
+            <TableCell style={{ width: "30%" }}>Message</TableCell>
+            <TableCell style={{ width: "10%" }}>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {contactRequests.length > 0 ? (
+            contactRequests.map((request, index) => (
+              <TableRow key={index}>
+                <TableCell>{request.name}</TableCell>
+                <TableCell>{request.phone_number}</TableCell>
+                <TableCell>{request.email}</TableCell>
+                <TableCell>{request.message}</TableCell>
+                <TableCell>
+                  <IconButton
+                    onClick={() => onDelete(index)}
+                    color="secondary"
+                  >
+                    <DeleteIcon />
+                  </IconButton>
                 </TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Grid>
-  );
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={5}>
+                <Typography variant="h6" color="error">
+                  {contactMeErrorMsg}
+                </Typography>
+              </TableCell>
+            </TableRow>
+          )}
+          {(contactMeErrorMsg && contactRequests.length > 0) &&
+            <TableRow>
+              <TableCell colSpan={5}>
+                <Alert severity="error">
+                  <AlertTitle>{"ERROR"}</AlertTitle>
+                  {contactMeErrorMsg}<strong>please try again!</strong>
+                </Alert>
+              </TableCell>
+            </TableRow>
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </Grid>
+);
 };
 
 export default ContactRequestsTable;
