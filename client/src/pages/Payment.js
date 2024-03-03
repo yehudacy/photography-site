6b16e13a900ff167e95355cf4382d0aea04b2dcc
yehudacy <126159312+yehudacy@users.noreply.github.com>
2024-03-03 18:27:58 +0200

    paypal implemnt non finished

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,55 +1,122 @@
-
 import React, { useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+// Renders errors or successful transactions on the screen.
+function Message({ content }) {
+  return <p>{content}</p>;
+}
+
 function Payment() {
   const initialOptions = {
-    "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID,
-    "disable-funding": "", 
+    "client-id": "ASrnNAjKgVJJIth-nv8wBmvwDFvjUemRep9q6J220ATs-QCchDsiDxPEduSHgA19uujk7zui_4roTLS-",
+    "enable-funding": "paylater,venmo,card",
+    "disable-funding": "",
+    "data-sdk-integration-source": "integrationbuilder_sc",
   };
 
   const [message, setMessage] = useState("");
-
-  const createOrder = async () => {
-    
-    // Implement your server logic to create a PayPal order
-    // You can use your Node.js server to make an API call to PayPal
-    // and return the order ID.
-    // Example:
-    // const response = await fetch("/api/orders", { method: "POST" });
-    // const orderData = await response.json();
-    // return orderData.id;
-
-    // For testing purposes, you can return a static order ID.
-    // return 5;
-  };
-
-  const onError = (err) => console.log(err);
-
-
-  const onApprove = async (data, actions) => {
-    // Implement your server logic to capture the PayPal payment
-    // Example:
-    // const response = await fetch(`/api/orders/${data.orderID}/capture`, { method: "POST" });
-    // const orderData = await response.json();
-
-    // For testing purposes, you can show a success message.
-    setMessage("Payment successful!");
-  };
 
   return (
     <div className="App">
       <PayPalScriptProvider options={initialOptions}>
         <PayPalButtons
           style={{
-            shape: "rect",
+            shape: "pill",
             layout: "vertical",
           }}
-          createOrder={createOrder}
-          onApprove={onApprove}
+          createOrder={async () => {
+            try {
+              const response = await fetch("/api/orders", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                // use the "body" param to optionally pass additional order information
+                // like product ids and quantities
+                body: JSON.stringify({
+                  cart: [
+                    {
+                      id: "YOUR_PRODUCT_ID",
+                    },
+                  ],
+                }),
+              });
+
+              const orderData = await response.json();
+
+              if (orderData.id) {
+                return orderData.id;
+              } else {
+                const errorDetail = orderData?.details?.[0];
+                const errorMessage = errorDetail
+                  ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+                  : JSON.stringify(orderData);
+
+                throw new Error(errorMessage);
+              }
+            } catch (error) {
+              console.error(error);
+              setMessage(`Could not initiate PayPal Checkout...${error}`);
+            }
+          }}
+
+          onError={(err) => {
+            console.log(err)
+          }}
+
+          onApprove={async (data, actions) => {
+            try {
+              const response = await fetch(
+                `/api/orders/${data.orderID}/capture`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                },
+              );
+
+              const orderData = await response.json();
+              // Three cases to handle:
+              //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+              //   (2) Other non-recoverable errors -> Show a failure message
+              //   (3) Successful transaction -> Show confirmation or thank you message
+
+              const errorDetail = orderData?.details?.[0];
+
+              if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                return actions.restart();
+              } else if (errorDetail) {
+                // (2) Other non-recoverable errors -> Show a failure message
+                throw new Error(
+                  `${errorDetail.description} (${orderData.debug_id})`,
+                );
+              } else {
+                // (3) Successful transaction -> Show confirmation or thank you message
+                // Or go to another URL:  actions.redirect('thank_you.html');
+                const transaction =
+                  orderData.purchase_units[0].payments.captures[0];
+                setMessage(
+                  `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`,
+                );
+                console.log(
+                  "Capture result",
+                  orderData,
+                  JSON.stringify(orderData, null, 2),
+                );
+              }
+            } catch (error) {
+              console.error(error);
+              setMessage(
+                `Sorry, your transaction could not be processed...${error}`,
+              );
+            }
+          }}
         />
       </PayPalScriptProvider>
-      <p>{message}</p>
+      <Message content={message} />
     </div>
   );
 }
