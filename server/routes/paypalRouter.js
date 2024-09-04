994733087b13ffef8a72294a1d541404d6489383
yehudacy <126159312+yehudacy@users.noreply.github.com>
2024-09-04 17:44:21 +0300

    start refactor paypal

--- server/routes/paypalRouter.js
+++ server/routes/paypalRouter.js
@@ -4,160 +4,32 @@ const qs = require("qs");
 const paypalRouter = express.Router();
 
 
-
-
-
-
-
-// import fetch from "node-fetch";
-// import "dotenv/config";
-// import path from "path";
-
-const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
-const base = "https://api-m.sandbox.paypal.com";
-// const app = express();
-
-// host static files
-// app.use(express.static("client"));
-
-// parse post params sent in body in json format
-// app.use(express.json());
-
-/**
- * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
- * @see https://developer.paypal.com/api/rest/authentication/
- */
-const generateAccessToken = async () => {
-  try {
-    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
-      throw new Error("MISSING_API_CREDENTIALS");
-    }
-    const auth = Buffer.from(
-      PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET,
-    ).toString("base64");
-    const {data} = await axios.post(`${base}/v1/oauth2/token`, qs.stringify({ 'grant_type': 'client_credentials' }),
-    {
-      headers: {
-        Authorization: `Basic ${auth}`,
-      },
-    });
-
-    // const data = await response.json();
-    return data.access_token;
-  } catch (error) {
-    console.error("Failed to generate Access Token:", error);
-  }
-};
-
-/**
- * Create an order to start the transaction.
- * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
- */
-const createOrder = async (cart) => {
-  // use the cart information passed from the front-end to calculate the purchase unit details
-  const price = cart.packagePrice == "gold" ? 1650 : cart.packagePrice == "platinum" ? 2400 : 1000;
-  console.log(
-    "shopping cart information passed from the frontend createOrder() callback:",
-    cart,
-  );
-
-  const accessToken = await generateAccessToken();
-  const url = `${base}/v2/checkout/orders`;
-  const payload = {
-    intent: "CAPTURE",
-    purchase_units: [
-      {
-        amount: {
-          currency_code: "USD",
-          value: price,
-        },
-      },
-    ],
-  };
-
-  const response = await axios.post(url, JSON.stringify(payload), {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
-      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
-      // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
-      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
-    },
-  });
-
-  return handleResponse(response);
-};
-
-/**
- * Capture payment for the created order to complete the transaction.
- * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
- */
-const captureOrder = async (orderID) => {
-  const accessToken = await generateAccessToken();
-  const url = `${base}/v2/checkout/orders/${orderID}/capture`;
-
-  const response = await axios.post(url, {}, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
-      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
-      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
-      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
-    },
-  });
-
-  return handleResponse(response);
-};
-
-async function handleResponse(response) {
-  try {
-    // const jsonResponse = await response.json();
-    const jsonResponse = await response.data;
-    return {
-      jsonResponse,
-      httpStatusCode: response.status,
-    };
-  } catch (err) {
-    const errorMessage = await response.text();
-    throw new Error(errorMessage);
-  }
-}
-
 paypalRouter.post("/", async (req, res) => {
-  try {
-    // use the cart information passed from the front-end to calculate the order amount details
-    const { cart } = req.body;
-    const { jsonResponse, httpStatusCode } = await createOrder(cart);
-    res.status(httpStatusCode).json(jsonResponse);
-  } catch (error) {
-    console.error("Failed to create order:", error);
-    res.status(500).json({ error: "Failed to create order." });
-  }
+  // try {
+  //   // use the cart information passed from the front-end to calculate the order amount details
+  //   const { cart } = req.body;
+  //   const { jsonResponse, httpStatusCode } = await createOrder(cart);
+  //   res.status(httpStatusCode).json(jsonResponse);
+  // } catch (error) {
+  //   console.error("Failed to create order:", error);
+  //   res.status(500).json({ error: "Failed to create order." });
+  // }
 });
 
 paypalRouter.post("/:orderID/capture", async (req, res) => {
-  try {
-    const { orderID } = req.params;
-    const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
-    res.status(httpStatusCode).json(jsonResponse);
-  } catch (error) {
-    console.error("Failed to create order:", error);
-    res.status(500).json({ error: "Failed to capture order." });
-  }
+  // try {
+  //   const { orderID } = req.params;
+  //   const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+  //   res.status(httpStatusCode).json(jsonResponse);
+  // } catch (error) {
+  //   console.error("Failed to create order:", error);
+  //   res.status(500).json({ error: "Failed to capture order." });
+  // }
 });
 
 // serve index.html
 paypalRouter.get("/", (req, res) => {
-  res.sendFile(path.resolve("./client/checkout.html"));
+  // res.sendFile(path.resolve("./client/checkout.html"));
 });
-
-// paypalRouter.listen(PORT, () => {
-//   console.log(`Node server listening at http://localhost:${PORT}/`);
-// });
-
 
 module.exports = {paypalRouter};
