7ebb50b0c4a87d6263c67b8fa09732841fa427e1
yehudacy <126159312+yehudacy@users.noreply.github.com>
2024-09-24 17:54:03 +0300

    fix padding issue in admin panel package page

--- client/src/pages/PackagePrices.js
+++ client/src/pages/PackagePrices.js
@@ -10,7 +10,11 @@ import {
   Typography,
 } from "@mui/material";
 import StarIcon from "@mui/icons-material/StarBorder";
-import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
+import {
+  Edit as EditIcon,
+  Delete as DeleteIcon,
+  AddCircle as AddIcon,
+} from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import axiosInstance from "../axiosInstance";
 
@@ -19,7 +23,6 @@ const PricingList = styled("ul")({
   padding: 0,
   listStyle: "none",
 });
-
 
 const PackagePrices = () => {
   const [editId, setEditId] = useState(null);
@@ -44,95 +47,105 @@ const PackagePrices = () => {
 
   return (
     <Grid
-      item
       container
       xs={12}
       md={9}
       sx={{
         margin: "auto",
-        padding: "16px",
         textAlign: "center",
         border: "2px solid #ccc",
         borderRadius: "8px",
         transition: "background-color 0.3s ease",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
         height: "100%",
-        marginTop: "104px", // Adjust this value to lower the form
+        width: "100%",
+        marginTop: "100px", // Adjust this value to lower the form
       }}
     >
-      <Typography variant="h6" sx={{ marginBottom: "16px" }}>
+      <Typography
+        variant="h5"
+        sx={{ marginBottom: "16px", width: "100%", textAlign: "center" }}
+      >
         Packages
       </Typography>
-      <Grid item container spacing={3} xs={12} sx={{ border: "2px solid lightblue" }}>
+      <Grid item container spacing={3} sx={{ margin: "auto" }}>
         {packages.map((pack) => {
-          return (<Grid
-            item
-            key={pack.package_id}
-            xs={12}
-            sm={pack.title === "Platinum" ? 12 : 6}
-            md={4}
-          >
-            <Card>
-              <CardHeader
-                title={pack.title}
-                titleTypographyProps={{ align: "center" }}
-                action={pack.title === "Gold" ? <StarIcon /> : null}
-                subheaderTypographyProps={{
-                  align: "center",
-                }}
-                sx={{
-                  backgroundColor: (theme) =>
-                    theme.palette.mode === "light"
-                      ? theme.palette.grey[200]
-                      : theme.palette.grey[700],
-                }}
-              />
-              <CardContent>
-                <Box
+          return (
+            <Grid
+              item
+              key={pack.package_id}
+              xs={12}
+              sm={6}
+              md={4}
+              sx={{ p: "24px" }}
+            >
+              <Card>
+                <CardHeader
+                  title={pack.title}
+                  titleTypographyProps={{ align: "center" }}
+                  action={pack.title === "Gold" ? <StarIcon /> : null}
+                  subheaderTypographyProps={{
+                    align: "center",
+                  }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === "light"
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "baseline",
+                      mb: 2,
+                    }}
+                  >
+                    <Typography
+                      component="h2"
+                      variant="h3"
+                      color="text.primary"
+                    >
+                      NIS{pack.price}
+                    </Typography>
+                  </Box>
+                  <PricingList>
+                    {pack.details.split(",").map((line) => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
+                        {line}
+                      </Typography>
+                    ))}
+                  </PricingList>
+                </CardContent>
+                <CardActions
                   sx={{
                     display: "flex",
+                    alignItems: "center",
                     justifyContent: "center",
-                    alignItems: "baseline",
-                    mb: 2,
                   }}
                 >
-                  <Typography component="h2" variant="h3" color="text.primary">
-                    NIS{pack.price}
-                  </Typography>
-                </Box>
-                <PricingList>
-                  {pack.details.split(",").map((line) => (
-                    <Typography
-                      component="li"
-                      variant="subtitle1"
-                      align="center"
-                      key={line}
-                    >
-                      {line}
-                    </Typography>
-                  ))}
-                </PricingList>
-              </CardContent>
-              <CardActions
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <IconButton onClick={handleEdit}>
-                  <EditIcon />
-                </IconButton>
-                <IconButton onClick={handleDelete}>
-                  <DeleteIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
-          </Grid>);
+                  <IconButton onClick={handleEdit}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={handleDelete}>
+                    <DeleteIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
         })}
+      </Grid>
+      <Grid item sx={{ width: "100%" }}>
+        <IconButton>
+          <AddIcon sx={{fontSize: '50px', }} color= 'primary'/>
+        </IconButton>
       </Grid>
     </Grid>
   );
