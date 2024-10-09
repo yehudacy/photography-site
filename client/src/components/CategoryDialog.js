a99ffbcd8b3550eb8d0cbaf254ca59d27525ac2a
yehudacy <126159312+yehudacy@users.noreply.github.com>
2024-10-09 18:35:15 +0300

    more on dialog

--- client/src/components/CategoryDialog.js
+++ client/src/components/CategoryDialog.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button, IconButton, Box, Typography } from '@mui/material';
+import { Clear, AddPhotoAlternate } from '@mui/icons-material';
 
 
 const btnStyle = {textTransform: 'none'};
@@ -24,6 +25,13 @@ const CategoryDialog = ({ open, onClose, handleSave, category }) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleImageChange = (e) => {
+    setFormData({ ...formData, src: e.target.value });
+  };
+
+  const handleImageClear = () => {
+    setFormData({ ...formData, src: '' });
+  };
 //   const handleSave = () => {
 //     onSave(formData);
 //     onClose();
@@ -43,15 +51,66 @@ const CategoryDialog = ({ open, onClose, handleSave, category }) => {
           value={formData.name}
           onChange={handleChange}
         />
-        <TextField
-          margin="dense"
-          name="src"
-          label="Image URL"
-          type="text"
-          fullWidth
-          value={formData.src}
-          onChange={handleChange}
-        />
+         <Box
+          sx={{
+            width: '100%',
+            height: '175px',
+            backgroundColor: formData.src ? 'transparent' : '#f0f0f0',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            border: '1px solid #ddd',
+            overflow: 'hidden',
+          }}
+        >
+          {formData.src ? (
+            <img
+              src={formData.src}
+              alt={formData.name}
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+              }}
+            />
+          ) : (
+            <Typography variant="body2" color="textSecondary">
+              No Image Available
+            </Typography>
+          )}
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+          {formData.src && (
+            <Button
+              variant="contained"
+              color="error"
+              sx={{...btnStyle, m : 1}}
+              onClick={handleImageClear}
+            >
+              <Clear />
+              <Typography variant="body2" color="inherit" sx={{ marginLeft: 1 }}>
+                Clear
+              </Typography>
+            </Button>
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{...btnStyle, m : 1}}
+            onClick={formData.src ? () => document.getElementById('image-input').click() : () => document.getElementById('image-input').click()}
+          >
+            <AddPhotoAlternate />
+            <Typography variant="body2" color="inherit" sx={{ marginLeft: 1 }}>
+              {formData.src ? 'Change Image' : 'Add Image'}
+            </Typography>
+          </Button>
+        </Box>
+          <input
+            type="file"
+            id="image-input"
+            style={{ display: 'none' }}
+            onChange={(e) => handleImageChange(e)}
+          />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} variant='contained' color="primary" sx={btnStyle}>
@@ -66,3 +125,28 @@ const CategoryDialog = ({ open, onClose, handleSave, category }) => {
 };
 
 export default CategoryDialog;
+
+{/* <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+{formData.src && (
+  <Button
+    variant="contained"
+    color="error"
+    onClick={handleImageClear}
+  >
+    <Clear />
+    <Typography variant="body2" color="inherit" sx={{ marginLeft: 1 }}>
+      Clear
+    </Typography>
+  </Button>
+)}
+<Button
+  variant="contained"
+  color="primary"
+  onClick={formData.src ? () => document.getElementById('image-input').click() : () => document.getElementById('image-input').click()}
+>
+  <AddPhotoAlternate />
+  <Typography variant="body2" color="inherit" sx={{ marginLeft: 1 }}>
+    {formData.src ? 'Change Image' : 'Add Image'}
+  </Typography>
+</Button>
+</Box> */}
